08e8caf89e1d167921a8c5240aa414f4523705d3
msa-campus <76155531+msa-campus@users.noreply.github.com>
2021-06-14 21:47:15 +0200

    fix stuff tilemap

---
+++ stuff 1 Kopie.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="stuff 1 Kopie" tilewidth="32" tileheight="32" tilecount="160" columns="8">
+ <image source="stuff 1 Kopie.png" width="256" height="640"/>
+</tileset>
